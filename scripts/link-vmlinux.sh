407a2c848e9779905aea0cb6a711fd291ab3fe8d
Hans Liljestrand <hans.goran.liljestrand@intel.com>
2023-09-01 01:56:54 -0700

    scripts: Fix compatibility with newer pahole

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -228,6 +228,10 @@ gen_btf()
 		extra_paholeopt="${extra_paholeopt} --btf_gen_floats"
 	fi
 
+	if [ "${pahole_ver}" -ge "124" ]; then
+		+	extra_paholeopt="${extra_paholeopt} --skip_encoding_btf_enum64"
+	fi
+
 	info "BTF" ${2}
 	LLVM_OBJCOPY="${OBJCOPY}" ${PAHOLE} -J ${extra_paholeopt} ${1}
 
